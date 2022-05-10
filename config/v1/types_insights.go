0295fdd234d8f0193c755a193c19e6480a97312d
Tomáš Remeš <tremes@redhat.com>
2022-05-10 13:37:11 +0200

    Just suggestion to make the Force attribute more powerful

--- config/v1/types_insights.go
+++ config/v1/types_insights.go
@@ -40,11 +40,13 @@ type GatheringConfig struct {
 	// in the Insights archive data.
 	// +kubebuilder:default=NoPolicy
 	DataPolicy DataPolicy `json:"dataPolicy"`
-	// ForceGatherReason enables user to force Insights data gathering by setting a new reason.
+	// forceRefreshReason enables user to force or refresh specific actions in the Insights operator.
+	// User can force the Insights data gathering by setting "Gathering" reason. User can also refresh
+	// a cluster transfer status by setting "ClusterTransfer" reason.
 	// When there is some gathering in the progress then it is interrupted.
 	// When all the gatherers are deactivated by the `DisabledGatherers`, nothing happens.
 	// When the forced gathering is finished then the value is cleared.
-	ForceGatherReason string `json:"forceGatherReason"`
+	ForceRefreshReason RefreshReason `json:"forceRefreshReason"`
 	// List of gatherers to be excluded from the gathering. All the gatherers can be disabled by providing "all" value.
 	// If all the gatherers are disabled, the Insights operator does not gather any data.
 	// The particular gatherers IDs can be found at https://github.com/openshift/insights-operator/blob/master/docs/gathered-data.md.
@@ -57,10 +59,18 @@ const (
 	NoPolicy DataPolicy = "NoPolicy"
 	// IP addresses and cluster domain name is obfuscated
 	IPsAndClusterDomainPolicy DataPolicy = "IPsAndClusterDomainPolicy"
+
+	Gathering RefreshReason = "Gathering"
+
+	ClusterTransfer RefreshReason = "ClusterTransfer"
+
+	Empty RefreshReason = "Empty"
 )
 
 // +kubebuilder:validation:Enum="";NoPolicy;IPsAndClusterDomainPolicy
 type DataPolicy string
+
+type RefreshReason string
 
 // Compatibility level 1: Stable within a major release for a minimum of 12 months or 3 minor releases (whichever is longer).
 // +openshift:compatibility-gen:level=1
